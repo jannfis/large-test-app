9d13278675829ac26cdc1f0129039893b308e4af
jannfis <jann@mistrust.net>
2021-06-16 10:50:53 +0000

    Hello, large input directory

---
+++ generate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for a in $(seq 1 1000); do
+	mkdir -p "src-$a"
+	for b in $(seq 1 5); do
+		cat >src-$a/manifest-$b.yaml <<_EOF_
+apiVersion: v1
+kind: ConfigMap
+metadata:
+	name: cm-dir$a-num$b
+_EOF_
+	done
+done
